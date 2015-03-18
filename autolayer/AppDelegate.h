9fbced9aca832cb923b9c929034a286985274090
xiaodongbie <xiaodongbie@users.noreply.github.com>
2015-03-19 00:00:44 +0800

    iphone adapter
    Support 4 4.7 4.7 3.5

---
+++ autolayer/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  autolayer
+//
+//  Created by D&T on 15/3/18.
+//  Copyright (c) 2015年 小东别. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ autolayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autolayer
+//
+//  Created by D&T on 15/3/18.
+//  Copyright (c) 2015年 小东别. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
